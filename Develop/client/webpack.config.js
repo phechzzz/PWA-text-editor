aba65c5173df4a7fc6fbe81c735df2d3d1d19072
phechz <peytonengborg@gmail.com>
2024-02-05 13:16:40 -0700

    install.js, database.js

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -19,17 +19,14 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      // Webpack plugin that generates our html file and injects our bundles
       new HtmlWebpackPlugin({
         template: './index.html',
         title: 'JATE'
       }),
-      // Injects our custom servie worker
       new InjectManifest({
         swSrc: './src-sw.js',
         swDest: 'src-sw.js',
       }),
-      // Creates a manifest.json file.
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
@@ -52,7 +49,6 @@ module.exports = () => {
     ],
 
     module: {
-      // CSS Loaders
       rules: [
         {
           test: /\.css$/i,
@@ -61,7 +57,6 @@ module.exports = () => {
         {
           test: /\.m?js$/,
           exclude: /node_modules/,
-          // We use babel-loader in order to use ES6.
           use: {
             loader: 'babel-loader',
             options: {
