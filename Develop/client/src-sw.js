45d33973e7e6efe62e115d32d0b845c854a937f2
phechz <peytonengborg@gmail.com>
2024-02-05 12:59:30 -0700

    service worker

--- Develop/client/src-sw.js
+++ Develop/client/src-sw.js
@@ -1,30 +1,56 @@
-const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
+// Import necessary Workbox modules
+const { warmStrategyCache } = require('workbox-recipes');
+const { CacheFirst, StaleWhileRevalidate } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
 const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
 
+// Precache and route URLs specified in the self.__WB_MANIFEST array
 precacheAndRoute(self.__WB_MANIFEST);
 
+// Define a CacheFirst strategy for caching pages
 const pageCache = new CacheFirst({
   cacheName: 'page-cache',
   plugins: [
+    // Cache responses with status codes 0 and 200
     new CacheableResponsePlugin({
       statuses: [0, 200],
     }),
+    // Set expiration for cached pages to 30 days
     new ExpirationPlugin({
       maxAgeSeconds: 30 * 24 * 60 * 60,
     }),
   ],
 });
 
+// Warm up the page cache with specific URLs
 warmStrategyCache({
   urls: ['/index.html', '/'],
   strategy: pageCache,
 });
 
-registerRoute(({ request }) => request.mode === 'navigate', pageCache);
+// Register a route for navigation requests using the pageCache strategy
+registerRoute(
+  ({ request }) => request.mode === 'navigate',
+  pageCache
+);
 
-// TODO: Implement asset caching
-registerRoute();
+// Set up asset cache for style, script, and worker files
+registerRoute(
+  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+  new StaleWhileRevalidate({
+    cacheName: 'asset-cache',
+    plugins: [
+      // Cache responses with status codes 0 and 200
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      // Set expiration for cached assets to 30 days
+      new ExpirationPlugin({
+        maxEntries: 60,
+        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+      }),
+    ],
+  })
+);
